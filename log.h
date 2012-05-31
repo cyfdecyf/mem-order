202884d647f0bafa6ae2d1453f1357006ab04e2a
Chen Yufei <cyfdecyf@gmail.com>
2012-05-31 22:52:44 +0800

    Set log buffer size to 4MB.

--- log.h
+++ log.h
@@ -6,7 +6,7 @@
 FILE *new_log(const char *name, long id);
 FILE *open_log(const char *name, long id);
 
-enum { LOG_BUFFER_SIZE = 4 * 1024 }; // For testing, make it small to make enlarge necessary.
+enum { LOG_BUFFER_SIZE = 4 * 1024 * 1024 }; // For testing, make it small to make enlarge necessary.
 
 void *open_mapped_log(const char *name, int id, int *fd);
 void *enlarge_mapped_log(void *buf, int fd);
