389c4b2065c67384e5e6b7d288e5ba18c2881168
Chen Yufei <cyfdecyf@gmail.com>
2012-07-21 19:37:50 +0800

    Fix mem-dummy compile error.

--- mem-dummy.c
+++ mem-dummy.c
@@ -1,14 +1,14 @@
 #include "mem.h"
 #include <stdio.h>
 
-int32_t mem_read(int32_t *addr) {
+int32_t mem_read(tid_t tid, int32_t *addr) {
     return *addr;
 }
 
-void mem_write(int32_t *addr, int32_t val) {
+void mem_write(tid_t tid, int32_t *addr, int32_t val) {
     *addr = val;
 }
 
-void mem_init(int nthr) {}
-void mem_init_thr(int tid) {}
+void mem_init(tid_t nthr) {}
+void mem_init_thr(tid_t tid) {}
 void mem_finish_thr() {}
