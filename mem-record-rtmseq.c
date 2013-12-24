1084b7dbbe19c658f34ec7778c156afee554d4a4
Chen Yufei <cyfdecyf@gmail.com>
2013-12-24 22:12:16 +0800

    Fix RTM bug.

--- mem-record-rtmseq.c
+++ mem-record-rtmseq.c
@@ -17,12 +17,33 @@ uint32_t mem_read(tid_t tid, uint32_t *addr) {
         // XXX TSX note: this transaction may execute and commit while writer is
         // inside fallback handler and has increased version by one. So the
         // writer's update to version and memory does not appear atomic to the
-        // reader.
-        tsx_assert((version & 1) == 0);
+        // reader. The assertion may fire, but it will abort checking lock in
+        // that case.
+        /*tsx_assert((version & 1) == 0);*/
         val = *addr;
+        // Check if lock is hold to avoid the problem mentioned above.
+        // Do this at the end of TX to lower abort rate.
+        //
+        // Suppose are using spinlock, if we check lock at start of the
+        // transaction, the following sequence will abort:
+        //
+        //     A       B
+        //   lock
+        //   ...
+        //   ...
+        //           xbegin
+        //           check lock
+        //   unlock
+        //
+        // The problem here is that unlock is a write which touches a single
+        // memory address. If we move the checking code to the end, and lock
+        // check happens after unlock, the transaction has chance to commit.
+        if (info->write_lock) {
+            _xabort(1);
+        }
         _xend();
     } else {
-        fprintf(stderr, "T%d R%ld fallback reason %d\n", g_tid, memop, ret);
+        fprintf(stderr, "T%d R%ld aborted %x, %d\n", g_tid, memop, ret, _XABORT_CODE(ret));
         do {
             version = info->version;
             while (unlikely(version & 1)) {
@@ -60,14 +81,20 @@ void mem_write(tid_t tid, uint32_t *addr, uint32_t val) {
     int ret = 0;
     if ((ret = _xbegin()) == _XBEGIN_STARTED) {
         version = info->version;
+        // XXX TSX note: same as read transaction, this may execute and commit
+        // while other writer is in fallback handler.
+        /*tsx_assert((version & 1) == 0);*/
         barrier();
         *addr = val;
         barrier();
         info->version += 2;
+        if (info->write_lock) {
+            _xabort(1);
+        }
         _xend();
         __sync_synchronize();
     } else {
-        fprintf(stderr, "T%d W%ld fallback reason %d\n", g_tid, memop, ret);
+        fprintf(stderr, "T%d W%ld aborted %x, %d\n", g_tid, memop, ret, _XABORT_CODE(ret));
         spin_lock(&info->write_lock);
 
         version = info->version;
